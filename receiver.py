abd9cd4e24636132ff428bc898e810ef6d6e1b3f
alextrax <luciferxiii@gmail.com>
2016-04-14 18:11:17 -0400

    Implement window size control

--- receiver.py
+++ receiver.py
@@ -1,7 +1,8 @@
 import sys
 import socket
-from struct import unpack
+from struct import unpack, pack
 
+buffer_size = 1024
 recv_packets = dict() # dictionary: key = seq, value = data_buffer
 
 def build_file(filename):
@@ -10,13 +11,27 @@ def build_file(filename):
     for i in sorted_index:
         f.write(recv_packets[i])
 
+def make_tcp_header(source_port, dst_port, seq, ack_seq, ack_flag, fin_flag, checksum, window_size):
+    size_of_header = 5 # 5 fields  
+    syn = 0
+    rst = 0
+    psh = 0
+    urg = 0
+    check = 0
+    urg_ptr = 0
+ 
+    header_length = (size_of_header << 4) + 0
+    tcp_flags = fin_flag + (syn << 1) + (rst << 2) + (psh <<3) + (ack_flag << 4) + (urg << 5)
+    return pack('!HHLLBBHHH' , source_port, dst_port, seq, ack_seq, header_length, tcp_flags,  window_size, checksum, urg_ptr)
+
 def handle_packet(header, payload):
     (source_port, dst_port, seq, ack_seq, header_length, tcp_flags,  window_size, checksum, urg_ptr)= unpack('!HHLLBBHHH' , header)
     print "source_port %d, dst_port %d, seq %d, ack_seq %d, header_length %d, tcp_flags %s,  window_size %d, checksum %d, tcp_urg_ptr %d" % (source_port, dst_port, seq, ack_seq, header_length >> 4, tcp_flags,  window_size, checksum, urg_ptr)
- 
-    print "received message:", payload
-    print len(payload) # should ack seq + len(payload) + 1
     recv_packets[seq] = payload
+
+    print len(payload) # should ack seq + len(payload) 
+    return seq
+    
 
 ''' receiver <filename> <listening_port> <sender_IP> <sender_port> <log_filename> '''
 def main():    
@@ -31,11 +46,14 @@ def main():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     sock.bind((host, listening_port))
+    ack_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 
     while True:
-        data, addr = sock.recvfrom(4096) 
-        handle_packet(data[:20], data[20:])
-
+        data, addr = sock.recvfrom(buffer_size) 
+        seq = handle_packet(data[:20], data[20:]) # header = data[:20], payload = data[20:]
+        ack_seq = seq + len(data[20:]) 
+        tcp_header = make_tcp_header(listening_port, sender_port, 0, ack_seq, 1, 0, 99, 5)
+        ack_sock.sendto(tcp_header, (sender_IP, sender_port))
 
 if __name__ == '__main__': 
     try:
--- sender.py
+++ sender.py
@@ -1,9 +1,11 @@
 import sys
 import socket
-from struct import pack
+import select 
+from struct import pack, unpack
 
-MSS = 16
-
+MSS = 128
+buffer_size = 1024
+current_sending = 0 # maintain # sending packets not to exceed window size
 all_packets = dict() # dictionary: key = seq, value = data_buffer
 sent_packets = dict() # dictionary: key = seq, value = time
 acked_packets = dict() # dictionary: key = seq, value = time
@@ -31,6 +33,12 @@ def make_tcp_header(source_port, dst_port, seq, ack_seq, ack_flag, fin_flag, che
     tcp_flags = fin_flag + (syn << 1) + (rst << 2) + (psh <<3) + (ack_flag << 4) + (urg << 5)
     return pack('!HHLLBBHHH' , source_port, dst_port, seq, ack_seq, header_length, tcp_flags,  window_size, checksum, urg_ptr)
 
+def handle_ack(header):
+    global current_sending
+    current_sending -= 1
+    (source_port, dst_port, seq, ack_seq, header_length, tcp_flags,  window_size, checksum, urg_ptr)= unpack('!HHLLBBHHH' , header)
+    print "ACK: source_port %d, dst_port %d, seq %d, ack_seq %d, header_length %d, tcp_flags %s,  window_size %d, checksum %d, tcp_urg_ptr %d" % (source_port, dst_port, seq, ack_seq, header_length >> 4, tcp_flags,  window_size, checksum, urg_ptr)
+ 
 
 ''' sender <filename> <remote_IP> <remote_port> <ack_port_num> <log_filename> <window_size> '''
 def main():    
@@ -45,16 +53,59 @@ def main():
     print "<filename>%s <remote_IP>%s <remote_port>%d <ack_port_num>%d <log_filename>%s <window_size>%s " % (filename, remote_ip, remote_port, ack_port_num, log_filename, window_size)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-    
-    #sock.sendto(tcp_header + "hello udp", (remote_ip, remote_port))
+    ack_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    ack_sock.bind((host, ack_port_num))
     build_all_packets("test.txt")
+
+
+    global current_sending
     sorted_index = sorted(all_packets)
-    for i in sorted_index:
-        tcp_header = make_tcp_header(ack_port_num, remote_port, i, 0, 0, 0, 99, 5)
-        sock.sendto(tcp_header + all_packets[i], (remote_ip, remote_port))
+    index_packets = 0
+    while current_sending < int(window_size):
+        if index_packets == len(sorted_index):
+                break
+        packet_seq = sorted_index[index_packets]
+        tcp_header = make_tcp_header(ack_port_num, remote_port, packet_seq, 0, 0, 0, 99, 5)
+        sock.sendto(tcp_header + all_packets[packet_seq], (remote_ip, remote_port))
+        print "SEND: seg = %d"%packet_seq
+        index_packets += 1
+        current_sending += 1
+
+    print "start select"
+    sockets_listen = [ack_sock] # socket list for select 
+    while True:
+        inputready,outputready,exceptready = select.select(sockets_listen,[],[]) 
+        for current in inputready:
+            if current == ack_sock: # receive ACK packet
+                data = current.recv(buffer_size) 
+                handle_ack(data[:20])
+
+        while current_sending < int(window_size):
+            if index_packets == len(sorted_index):
+                break
+            packet_seq = sorted_index[index_packets]
+            tcp_header = make_tcp_header(ack_port_num, remote_port, packet_seq, 0, 0, 0, 99, 5)
+            sock.sendto(tcp_header + all_packets[packet_seq], (remote_ip, remote_port))
+            print "SEND: seg = %d"%packet_seq
+            index_packets += 1
+            current_sending += 1
 
 if __name__ == '__main__': 
     try:
         main()
     except KeyboardInterrupt:
         print '\nserver receive ctrl+C\n'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
