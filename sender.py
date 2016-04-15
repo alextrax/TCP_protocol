defb83af3a0a8d25ad69f7f400fbc84344ecad21
alextrax <luciferxiii@gmail.com>
2016-04-14 22:48:04 -0400

    Implement timeout and resend packet

--- sender.py
+++ sender.py
@@ -1,12 +1,16 @@
 import sys
 import socket
 import select 
+import threading
 from struct import pack, unpack
 from datetime import datetime
 
 MSS = 128
 buffer_size = 1024
 ERTT = 0
+timeout = 5 # 5 secs
+last_packet_size = 0
+final_seq = 0
 current_sending = 0 # maintain # sending packets not to exceed window size
 all_packets = dict() # dictionary: key = seq, value = data_buffer
 sent_packets = dict() # dictionary: key = seq, value = time
@@ -15,11 +19,13 @@ acked_packets = dict() # dictionary: key = seq, value = time
 def build_all_packets(file):
     f = open(file)
     seq = 0
+    global last_packet_size
     while True:
         chunk = f.read(MSS)
         if chunk == "":
             break
         all_packets[seq] = chunk
+        last_packet_size = len(chunk)
         seq += MSS
 
 def make_tcp_header(source_port, dst_port, seq, ack_seq, ack_flag, fin_flag, checksum, window_size):
@@ -54,9 +60,32 @@ def handle_ack(header, log_filename):
         acked_packets[ack_seq] = datetime.now()
         current_sending -= 1
     else: # duplicate ack
-        print "duplicate ack: %d" % ack_seq  
+        print "duplicate ack: %d\n" % ack_seq  
 
+    if len(acked_packets) == len(all_packets):
+        print "File transfer finish!"
     return ack_seq
+
+def timeout_checker(log_filename, sock, ack_port_num, remote_ip, remote_port, packet_seq, window_size):
+    if packet_seq == final_seq:
+        check_seq = packet_seq + last_packet_size
+    else:
+        check_seq = packet_seq + MSS    
+
+    #print "timeout_checker check %d for seq %d " % (check_seq, packet_seq)
+    
+    if check_seq not in acked_packets: # packet lost timeout  
+        tcp_header = make_tcp_header(ack_port_num, remote_port, packet_seq, 0, 0, 0, 99, window_size)
+        sock.sendto(tcp_header + all_packets[packet_seq], (remote_ip, remote_port))
+        checker_register(log_filename, sock, ack_port_num, remote_ip, remote_port, packet_seq, window_size) # register a timeout checker
+        print "timeout! check seq %d failed, resend seq: %d\n" % (check_seq, packet_seq)
+        write_log(log_filename, ack_port_num, remote_port, packet_seq, 0, 0)
+
+def checker_register(log_filename, sock, ack_port_num, remote_ip, remote_port, packet_seq, window_size):
+    t = threading.Timer(timeout, timeout_checker, [log_filename, sock, ack_port_num, remote_ip, remote_port, packet_seq, window_size])
+    t.daemon = True
+    t.start()
+    
 
 ''' sender <filename> <remote_IP> <remote_port> <ack_port_num> <log_filename> <window_size> '''
 def main():    
@@ -67,6 +96,7 @@ def main():
     ack_port_num = int(sys.argv[4])
     log_filename = sys.argv[5]
     window_size = sys.argv[6]
+    global final_seq
 
     print "<filename>%s <remote_IP>%s <remote_port>%d <ack_port_num>%d <log_filename>%s <window_size>%s " % (filename, remote_ip, remote_port, ack_port_num, log_filename, window_size)
 
@@ -78,18 +108,20 @@ def main():
 
     global current_sending
     sorted_index = sorted(all_packets)
+    final_seq = sorted_index[-1]
     index_packets = 0
     while current_sending < int(window_size):
         if index_packets == len(sorted_index):
                 break
         packet_seq = sorted_index[index_packets]
-        tcp_header = make_tcp_header(ack_port_num, remote_port, packet_seq, 0, 0, 0, 99, 5)
+        tcp_header = make_tcp_header(ack_port_num, remote_port, packet_seq, 0, 0, 0, 99, int(window_size))
         sock.sendto(tcp_header + all_packets[packet_seq], (remote_ip, remote_port))
+        checker_register(log_filename, sock, ack_port_num, remote_ip, remote_port, packet_seq, int(window_size)) # register a timeout checker
         write_log(log_filename, ack_port_num, remote_port, packet_seq, 0, 0) 
         index_packets += 1
         current_sending += 1
 
-    print "start select"
+    print "start select, final_seq = %d" % final_seq
     sockets_listen = [ack_sock] # socket list for select 
     while True:
         inputready,outputready,exceptready = select.select(sockets_listen,[],[]) 
@@ -102,8 +134,9 @@ def main():
             if index_packets == len(sorted_index):
                 break
             packet_seq = sorted_index[index_packets]
-            tcp_header = make_tcp_header(ack_port_num, remote_port, packet_seq, 0, 0, 0, 99, 5)
+            tcp_header = make_tcp_header(ack_port_num, remote_port, packet_seq, 0, 0, 0, 99, int(window_size))
             sock.sendto(tcp_header + all_packets[packet_seq], (remote_ip, remote_port)) 
+            checker_register(log_filename, sock, ack_port_num, remote_ip, remote_port, packet_seq, int(window_size)) # register a timeout checker
             write_log(log_filename, ack_port_num, remote_port, packet_seq, 0, 0) 
             index_packets += 1
             current_sending += 1
@@ -112,8 +145,8 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        for i in sorted(acked_packets):
-            print i
+        print len(all_packets)
+        print len(acked_packets)
         print '\nserver receive ctrl+C\n'
 
 
